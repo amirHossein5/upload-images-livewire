7060f13654235b486a804de467183bdf20eab3e9
Amir Hosein <adada5116@gmail.com>
2021-07-15 15:11:22 +0430

    first

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('-','_',app()->getLocale()) }}">
+
+<head>
+    @include('layouts.head-tag')
+    @yield('styles')
+</head>
+
+<body class="bg-gray-800 min-h-screen">
+
+    @yield('content')
+
+    @include('layouts.scripts')
+    @yield('scripts')
+
+</body>
+
+</html>
---
+++ resources/views/layouts/scripts.blade.php
@@ -0,0 +1,7 @@
+{{-- BEGIN: scripts --}}
+<script src="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/11.0.17/sweetalert2.all.min.js"></script>
+
+@livewireScripts()
+
+<script src="{{ mix('js/app.js') }}"></script>
+{{-- END: scripts --}}
